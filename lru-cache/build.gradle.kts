ecdeb77380c24342d8e62f9fb53542fd35a9e61e
nikita <nikita.kochetkov@jetbrains.com>
2020-09-25 03:02:21 +0300

    init lru-cache

---
+++ lru-cache/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.4.0"
+}
+
+group = "ru.ifmo.kochetkov"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ lru-cache/src/main/kotlin/ru/ifmo/kochetkov/Main.kt
@@ -0,0 +1,5 @@
+package ru.ifmo.kochetkov
+
+fun main() {
+    println("Hello world!")
+}
