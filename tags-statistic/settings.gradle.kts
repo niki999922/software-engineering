a5ab7c6a003a79adc1a05899e1998a8dc657a6de
nikita <nikita.kochetkov@jetbrains.com>
2020-10-27 02:32:05 +0300

    solve statistic and init refactor

--- tags-statistic/settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-rootProject.name = "tags-statistic"
-
--- tags-statistic/src/main/kotlin/ru/ifmo/kochetkov/LRUCache.kt
+++
@@ -1,7 +0,0 @@
-package ru.ifmo.kochetkov
-
-interface LRUCache<K, V> {
-    fun refresh(key: K): LRUCache<K, V>
-    fun insert(key: K, value: V): LRUCache<K, V>
-    fun get(key: K): V?
-}
